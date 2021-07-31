ee44af9a591a999acb44a3c57fb9799523bb5267
SATVIK2204 <bksatvik@gmail.com>
2021-07-31 20:53:56 +0530

    Edited routes

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,9 @@ router.get("/dashboard", ensureAuth, async (req, res) => {
 
   try {
     const stories = await Story.find({ user : req.user.id}).lean()
-
+    res.render("dashboard", {
+      name : req.user.firstName,
+      stories
     });
   } catch (err){
     console.error(err)
